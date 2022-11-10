52090e6c7c80e630df244d65842edbced6a2f988
Ho Van Hoa <hoa.ho.van@hcmut.edu.vn>
2022-11-10 18:51:35 +0700

    Init Proj

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import LoginComponents from './Components/Login'
+
+function App() {
+  return (
+    <div className="container">
+      <LoginComponents/>
+    </div>
+  );
+}
+
+export default App;
